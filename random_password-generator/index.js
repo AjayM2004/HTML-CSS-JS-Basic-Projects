1ee84612dbb3ad3acf90004bb4385a7f1e3b4bdb
Ajay M <ajaym@indiaadvocacy.com>
2025-01-31 12:06:04 +0530

    Create index.js

---
+++ random_password-generator/index.js
@@ -0,0 +1,13 @@
+function generatePassword(isDigit,isUpperCase,isLowerCase,isSymbols,len){
+
+   return;
+}
+console.log("Ajay");
+let isDigit = true;
+let isUpperCase = true;
+let isLowerCase = true;
+let isSymbols = true;
+let len = 8;
+console.log(Math.floor(3.7));
+console.log(Math.ceil(3.7));
+console.log(Math.floor(Math.random()*100));
